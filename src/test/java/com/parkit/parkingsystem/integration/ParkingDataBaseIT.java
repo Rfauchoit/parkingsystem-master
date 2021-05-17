d1cb00d50a0d300c38ba3405968e40c63f4d8c07
Randy Fauchoit <randy.fauchoit@gmail.com>
2021-05-17 16:05:51 +0200

    Ecriture test d'integration

--- src/test/java/com/parkit/parkingsystem/integration/ParkingDataBaseIT.java
+++ src/test/java/com/parkit/parkingsystem/integration/ParkingDataBaseIT.java
@@ -1,6 +1,6 @@
 package com.parkit.parkingsystem.integration;
 
-import com.parkit.parkingsystem.config.DataBaseConfig;
+import com.parkit.parkingsystem.integration.config.DataBaseTestConfig;
 import com.parkit.parkingsystem.constants.ParkingType;
 import com.parkit.parkingsystem.dao.ParkingSpotDAO;
 import com.parkit.parkingsystem.dao.TicketDAO;
@@ -22,7 +22,7 @@ import static org.junit.jupiter.api.Assertions.*;
 @ExtendWith(MockitoExtension.class)
 public class ParkingDataBaseIT {
 
-    private static DataBaseConfig dataBaseConfig = new DataBaseConfig();
+    private static DataBaseTestConfig dataBaseConfig = new DataBaseTestConfig();
     private static ParkingSpotDAO parkingSpotDAO;
     private static TicketDAO ticketDAO;
     private static DataBasePrepareService dataBasePrepareService;
@@ -54,7 +54,6 @@ public class ParkingDataBaseIT {
     @Test
     public void testParkingACar() throws Exception{
     	// Act
-    	dataBasePrepareService.clearDataBaseEntries();
     	ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
         parkingService.processIncomingVehicle();
         Ticket ticket = ticketDAO.getTicket(inputReaderUtil.readVehicleRegistrationNumber());
@@ -65,7 +64,7 @@ public class ParkingDataBaseIT {
         assertEquals("ABCDEF", ticket.getVehicleRegNumber());
         assertNotNull(ticket.getInTime());
         assertNull(ticket.getOutTime());
-        assertEquals(1,parkingSpotDAO.getNextAvailableSlot(ParkingType.CAR));
+        assertEquals(2,parkingSpotDAO.getNextAvailableSlot(ParkingType.CAR));
        
      
    
@@ -75,16 +74,16 @@ public class ParkingDataBaseIT {
     public void testParkingLotExit() throws Exception{
     	//TODO: check that the fare generated and out time are populated correctly in 
     	// the database
+    	
     	// Act
 
         ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
-        //parkingService.processIncomingVehicle();
-        //Thread.sleep(100);
+        parkingService.processIncomingVehicle();
         parkingService.processExitingVehicle();
         Ticket ticket = ticketDAO.getTicket(inputReaderUtil.readVehicleRegistrationNumber());
         // Assert//
         assertEquals(0, ticket.getPrice());
-        //assertTrue(ticket.getOutTime())>ticket.getInTime();
+        assertTrue(ticket.getOutTime().compareTo(ticket.getInTime())>=0);
     }
 
 }
